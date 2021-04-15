022ad029b04e13cf87ac49845d768957e318577c
hrlnsnchz <hrlnsnchz@gmail.com>
2021-04-15 11:25:20 -0500

    Deleted an unnecessary PUT function

--- request_handler.py
+++ request_handler.py
@@ -126,8 +126,6 @@ class HandleRequests(BaseHTTPRequestHandler):
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
-    def do_PUT(self):
-        self.do_POST()
 
 
     def do_PUT(self):
