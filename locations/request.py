206cec409ee0c6b10f4a15bdc9390cf07dc4a1fe
hrlnsnchz <hrlnsnchz@gmail.com>
2021-04-18 11:55:31 -0500

    Added a function to parse query parameters for customer emails

--- locations/request.py
+++ locations/request.py
@@ -20,23 +20,23 @@ LOCATIONS = [
   ]
 
 
-def get_all_locations():
-    return LOCATIONS
+# def get_all_locations():
+#     return LOCATIONS
 
-    # Function with a single parameter
-def get_single_location(id):
-    # Variable to hold the found location, if it exists
-    requested_location = None
+#     # Function with a single parameter
+# def get_single_location(id):
+#     # Variable to hold the found location, if it exists
+#     requested_location = None
 
-    # Iterate the locations list above. Very similar to the
-    # for..of loops you used in JavaScript.
-    for location in LOCATIONS:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if location["id"] == id:
-            requested_location = location
+#     # Iterate the locations list above. Very similar to the
+#     # for..of loops you used in JavaScript.
+#     for location in LOCATIONS:
+#         # Dictionaries in Python use [] notation to find a key
+#         # instead of the dot notation that JavaScript used.
+#         if location["id"] == id:
+#             requested_location = location
 
-    return requested_location
+#     return requested_location
 
 def create_location(location):
     # Get the id value of the last location in the list
